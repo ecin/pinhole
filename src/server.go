6c7ff5cb3b98073631f2cec0751dc9825fa01c30
ecin <ecin@copypastel.com>
2013-06-12 17:47:26 -0700

    Spike!
    We can take screenshots of arbitrary sites now; time for screenshot is
    mostly dependent on page load time.

---
+++ src/server.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+  "os/exec"
+  "net/http"
+  "log"
+  "os"
+  "path"
+//  "fmt"
+)
+
+const (
+  SCREENSHOT_PATH = "/tmp"
+  TWITTER_CARD_URL = "http://localhost:3000/twitter_cards/"
+)
+
+func screenshotPath(opaqueId string) string {
+  return path.Join(SCREENSHOT_PATH, opaqueId + ".png")
+}
+
+func twitterCardHandler(response http.ResponseWriter, request *http.Request) {
+  opaqueId := request.URL.Path[len("/cards/"):]
+  outputFile := screenshotPath(opaqueId)
+  url := TWITTER_CARD_URL + opaqueId
+
+  phantomJS := exec.Command("bin/screenshot", url, outputFile)
+  cached := true
+
+  if _, err := os.Stat(screenshotPath(opaqueId)); os.IsNotExist(err) {
+    cached = false
+  }
+
+  // redirect to file if everything is fine
+  if !cached && phantomJS.Run() != nil {
+    response.WriteHeader(http.StatusInternalServerError)
+  } else {
+    response.Header().Set("Location", "/static/" + opaqueId + ".png")
+    response.WriteHeader(http.StatusFound)
+  }
+}
+
+func Log(handler http.Handler) http.Handler {
+  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+    handler.ServeHTTP(w, r)
+  })
+}
+
+func main() {
+  http.HandleFunc("/cards/", twitterCardHandler)
+  http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir(SCREENSHOT_PATH))))
+  http.ListenAndServe(":8080", Log(http.DefaultServeMux))
+}
